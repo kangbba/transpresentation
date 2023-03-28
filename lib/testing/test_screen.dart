8637474d47888b5cce991fefc20a66bd09789aa1
kangbba <121343882+kangbba@users.noreply.github.com>
2023-03-28 19:30:24 +0900

    preparation for iOS

--- lib/testing/test_screen.dart
+++ lib/testing/test_screen.dart
@@ -1,144 +1,77 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:google_speech/google_speech.dart';
-import 'package:path_provider/path_provider.dart';
-class AudioRecognize extends StatefulWidget {
+import 'package:speech_to_text/speech_to_text.dart' as stt;
+
+class TestScreen extends StatefulWidget {
   @override
-  State<StatefulWidget> createState() => _AudioRecognizeState();
+  _TestScreenState createState() => _TestScreenState();
 }
 
-class _AudioRecognizeState extends State<AudioRecognize> {
-  bool recognizing = false;
-  bool recognizeFinished = false;
-  String text = '';
-  final jsonPath = 'assets/quick-woodland-376602-7a68644c54ac.json';
+class _TestScreenState extends State<TestScreen> {
+  late stt.SpeechToText _speech;
+  bool _isListening = false;
+  String _text = '';
 
-  void recognize() async {
-    setState(() {
-      recognizing = true;
-    });
-    final serviceAccount = ServiceAccount.fromString(
-        '${(await rootBundle.loadString(jsonPath))}');
-    final speechToText = SpeechToText.viaServiceAccount(serviceAccount);
-    final config = _getConfig();
-    final audio = await _getAudioContent('test.wav');
-
-    await speechToText.recognize(config, audio).then((value) {
-      setState(() {
-        text = value.results
-            .map((e) => e.alternatives.first.transcript)
-            .join('\n');
-      });
-    }).whenComplete(() => setState(() {
-      recognizeFinished = true;
-      recognizing = false;
-    }));
-  }
-
-  void streamingRecognize() async {
-    setState(() {
-      recognizing = true;
-    });
-    final serviceAccount = ServiceAccount.fromString(
-        '${(await rootBundle.loadString(jsonPath))}');
-    final speechToText = SpeechToText.viaServiceAccount(serviceAccount);
-    final config = _getConfig();
-
-    final responseStream = speechToText.streamingRecognize(
-        StreamingRecognitionConfig(config: config, interimResults: true),
-        await _getAudioStream('test.wav'));
-
-    responseStream.listen((data) {
-      setState(() {
-        text =
-            data.results.map((e) => e.alternatives.first.transcript).join('\n');
-        recognizeFinished = true;
-      });
-    }, onDone: () {
-      setState(() {
-        recognizing = false;
-      });
-    });
-  }
-
-  RecognitionConfig _getConfig() => RecognitionConfig(
-      encoding: AudioEncoding.LINEAR16,
-      model: RecognitionModel.basic,
-      enableAutomaticPunctuation: true,
-      sampleRateHertz: 16000,
-      languageCode: 'en-US');
-
-  Future<void> _copyFileFromAssets(String name) async {
-    var data = await rootBundle.load('assets/$name');
-    final directory = await getApplicationDocumentsDirectory();
-    final path = directory.path + '/$name';
-    await File(path).writeAsBytes(
-        data.buffer.asUint8List(data.offsetInBytes, data.lengthInBytes));
-  }
-
-  Future<List<int>> _getAudioContent(String name) async {
-    final directory = await getApplicationDocumentsDirectory();
-    final path = directory.path + '/$name';
-    if (!File(path).existsSync()) {
-      await _copyFileFromAssets(name);
-    }
-    return File(path).readAsBytesSync().toList();
-  }
-
-  Future<Stream<List<int>>> _getAudioStream(String name) async {
-    final directory = await getApplicationDocumentsDirectory();
-    final path = directory.path + '/$name';
-    if (!File(path).existsSync()) {
-      await _copyFileFromAssets(name);
-    }
-    return File(path).openRead();
+  @override
+  void initState() {
+    super.initState();
+    _speech = stt.SpeechToText();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Audio File Example'),
+        title: Text('Speech to Text Test'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: <Widget>[
-            if (recognizeFinished)
-              Column(
-                children: <Widget>[
-                  Text(
-                    'The text recognized by the Google Speech Api:',
-                  ),
-                  SizedBox(
-                    height: 16.0,
-                  ),
-                  Text(
-                    text,
-                    style: Theme.of(context).textTheme.bodyText1,
-                  ),
-                ],
+      body: Column(
+        children: [
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.all(20.0),
+              child: Text(
+                _text,
+                style: TextStyle(fontSize: 24.0),
               ),
-            ElevatedButton(
-              onPressed: recognizing ? () {} : recognize,
-              child: recognizing
-                  ? CircularProgressIndicator()
-                  : Text('Test with recognize'),
             ),
-            SizedBox(
-              height: 10.0,
+          ),
+          FloatingActionButton(
+            onPressed: _listen,
+            child: Icon(
+              _isListening ? Icons.mic : Icons.mic_none,
             ),
-            ElevatedButton(
-              onPressed: recognizing ? () {} : streamingRecognize,
-              child: recognizing
-                  ? CircularProgressIndicator()
-                  : Text('Test with streaming recognize'),
-            ),
-          ],
-        ),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+          ),
+        ],
+      ),
     );
+  }
+
+  void _listen() async {
+    if (!_isListening) {
+      bool available = await _speech.initialize(
+        onStatus: (status) {
+          print('status: $status');
+        },
+        onError: (error) {
+          print('error: $error');
+        },
+      );
+      if (available) {
+        setState(() {
+          _isListening = true;
+        });
+        _speech.listen(
+          onResult: (result) {
+            setState(() {
+              _text = result.recognizedWords;
+            });
+          },
+        );
+      }
+    } else {
+      setState(() {
+        _isListening = false;
+      });
+      _speech.stop();
+    }
   }
 }
